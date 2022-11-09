6c7749cc054d0bdc69f206d4f7aa723c24d9f8ef
ben.li <53158397+beni-code@users.noreply.github.com>
2022-11-09 14:27:01 +0800

    21

--- main.js
+++ main.js
@@ -1,21 +1,25 @@
 const fs = require('fs')
 const path = require('path')
+const fsExtra = require('fs-extra')
 
 const imgs = fs.readdirSync('./imgs')
 
-let pngs =  imgs.filter(img=>{
+let pngs = imgs.filter(img => {
     if (path.extname(img) === '.png') {
         return true;
-    }else{
+    } else {
         return false;
     }
-}).map(png=>{
+}).map(png => {
     return `./imgs/${png}`
 })
 
 let str = `let pngs = ${JSON.stringify(pngs)}`
 
-fs.mkdirSync('./dist')
-fs.writeFileSync('./dist/index.js',str,'utf-8')
-fs.copyFileSync('./index.html','./dist/index.html')
-fs.cp('./imgs/','./dist/')
+if (!fs.existsSync('./dist')) {
+
+    fs.mkdirSync('./dist')
+}
+fs.writeFileSync('./dist/index.js', str, 'utf-8')
+fs.copyFileSync('./index.html', './dist/index.html')
+fsExtra.copySync('./imgs', './dist/imgs')
